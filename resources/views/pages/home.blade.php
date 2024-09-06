f887eda50a5df398ffd6ede1d8aa67ea70ab6eb5
Abdurrahman Harmankaya <abdurrahman.kim@gmail.com>
2024-09-06 23:45:30 +0300

    Update home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-visionary::layouts>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
@@ -8,4 +8,4 @@
     {{--<x-visionary::carousel></x-visionary::carousel>--}}
 
     Merhaba
-</x-app-layout>
+</x-visionary::layouts>
